9f808ef3a78c249dd14d72f45961adb653c89c1d
Joshua Nugraha <30294923+JoshuaNugraha@users.noreply.github.com>
2022-04-17 01:09:44 +0800

    Add files via upload

---
+++ application/config/Facebook.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+$config['facebook']['app_id'] = '546487213509092'; //masukkan app id disini
+$config['facebook']['app_secret'] = 'e12bafe46daba623f8c54216797fd599'; //masukkan app secret disini
+$config['facebook']['default_graph_version'] = 'v2.10';
